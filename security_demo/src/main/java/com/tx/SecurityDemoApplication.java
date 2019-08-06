049c5d6861322eefc931c6427f61a4f767663664
mercuryTan <mercuryTan@163.com>
2019-08-06 22:57:14 +0800

    hello world

---
+++ security_demo/src/main/java/com/tx/SecurityDemoApplication.java
@@ -0,0 +1,23 @@
+package com.tx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@SpringBootApplication
+@RestController
+public class SecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoApplication.class,args);
+    }
+
+    @GetMapping
+    public String hello(){
+        return "hello world";
+    }
+}
